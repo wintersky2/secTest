11b85eea048a5d79ae722956a49d6f102aba0265
wintersky2 <emsw747@gmail.com>
2024-01-09 14:10:58 +0900

    1st

---
+++ src/main/java/com/example/secTest/SecTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.secTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/secTest/SecTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
